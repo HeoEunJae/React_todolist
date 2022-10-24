33272f8bb4ebd64959417e20180f16171c434fe0
HeoEunJae <dmswo159@gmail.com>
2022-10-24 23:45:19 +0900

    할일 추가

---
+++ src/components/TodoList.jsx
@@ -0,0 +1,58 @@
+import React from 'react';
+import TodoListItem from './TodoListItem';
+
+const TodoList = ({todos}) => {
+  return (
+    <div>
+      <div class="overflow-x-auto">
+        <table class="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>할 일</th>
+              <th>완료 상태</th>
+            </tr>
+          </thead>
+          <tbody>
+            {todos.map((todo, index)=> <TodoListItem />)}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default TodoList;<div class="overflow-x-auto">
+        <table class="table w-full">
+          <thead>
+            <tr>
+              <th></th>
+              <th>Name</th>
+              <th>Job</th>
+              <th>Favorite Color</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>1</th>
+              <td>Cy Ganderton</td>
+              <td>Quality Control Specialist</td>
+              <td>Blue</td>
+            </tr>
+
+            <tr>
+              <th>2</th>
+              <td>Hart Hagerty</td>
+              <td>Desktop Support Technician</td>
+              <td>Purple</td>
+            </tr>
+
+            <tr>
+              <th>3</th>
+              <td>Brice Swyre</td>
+              <td>Tax Accountant</td>
+              <td>Red</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
---
+++ src/components/TodoListItem.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TodoListItem = () => {
+  return (
+     <tr>
+        <th>1</th>
+        <td>기상</td>
+        <td>true</td>
+      </tr>  
+  );
+};
+
+export default TodoListItem;
